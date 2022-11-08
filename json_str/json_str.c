c118a0598b0f9bb4b5073c441cdc8517cf19b0c6
Vasily Davydov <vasily.davydov@metropolia.fi>
2022-11-09 00:48:53 +0200

    json_str: format and add doc (#6)
    * format to be project cohesive
    * add public api documentation

--- json_str/json_str.c
+++ json_str/json_str.c
@@ -3,157 +3,220 @@
 //
 #include "json_str.h"
 
-static bool compare_str(const char *str1, const char *str2) {
-    while (*str1 && *str2) {
-        if (*str1 != *str2) return false;
-        str1++;
-        str2++;
-    }
-    return (*str2 == 0);
-}
-
-static const char* strstr(const char* haystack, const char* needle)
+static bool
+j_compare_str (const char *str1, const char *str2)
 {
-    while (*haystack != '\0')
+  while (*str1 && *str2)
     {
-        if ((*haystack == *needle) && compare_str(haystack, needle)) {
-            return haystack;
+      if (*str1 != *str2)
+        return false;
+      str1++;
+      str2++;
+    }
+  return (*str2 == 0);
+}
+
+static const char *
+j_strstr (const char *haystack, const char *needle)
+{
+  while (*haystack != '\0')
+    {
+      if ((*haystack == *needle) && j_compare_str (haystack, needle))
+        {
+          return haystack;
         }
-        haystack++;
+      haystack++;
     }
 
-    return NULL;
+  return NULL;
 }
 
-static unsigned int strlen(const char * str){
-    unsigned int cc = 0;
-    for (; *str != 0; str++){
-        cc++;
+static unsigned int
+j_strlen (const char *str)
+{
+  unsigned int cc = 0;
+  for (; *str != 0; str++)
+    {
+      cc++;
     }
-    return cc;
+  return cc;
 }
 
-static void memorycpy(void * from,void * to, unsigned int n){
-    if(!from) return;
-    char * source = (char*)from;
-    char * dest = (char*)to;
-    while(n){
-        (*dest++)=(*source++);
-        n--;
+static void
+j_memcpy (void *from, void *to, unsigned int n)
+{
+  if (!from)
+    return;
+  char *source = (char *)from;
+  char *dest = (char *)to;
+  while (n)
+    {
+      (*dest++) = (*source++);
+      n--;
     }
 }
 
-static void memoryset(void * data, char c, int n){
-    if(!data) return;
-    char* c_data = (char*)data;
-    for(int i = 0; i < n; i++){
-        c_data[i] = c;
+static void
+j_memset (void *data, char c, int n)
+{
+  if (!data)
+    return;
+  char *c_data = (char *)data;
+  for (int i = 0; i < n; i++)
+    {
+      c_data[i] = c;
     }
 }
 
-static bool isspace(char s){
-    switch (s) {
-        case ' ':
-            break;
-        case '\n':
-            break;
-        case '\r':
-            break;
-        case '\t':
-            break;
-        case '\v':
-            break;
-        case '\f':
-            break;
-        default:
-            return false;
+static bool
+j_isspace (char s)
+{
+  switch (s)
+    {
+    case ' ':
+      break;
+    case '\n':
+      break;
+    case '\r':
+      break;
+    case '\t':
+      break;
+    case '\v':
+      break;
+    case '\f':
+      break;
+    default:
+      return false;
     }
-    return true;
+  return true;
 }
 
 /*
-* Clears received json string from whitespaces and newlines. Doesn't affect variable scopes.
-* ( '{ "make" : "My Car" }' -> '{"make":"My Car"}' )
-*/
-static void clean_json_string(char **string) {
-    if (*string == JSON_STR_NULL) return; //Received null pointer.
-    bool var_scope = false; //Flag to check whether whitespace is located inside the variable scope. (We don't want to ruin the names)
-    int i = 0;
-    int w = strlen(*string);
-    char prev_char = ' '; //Not the best way to check '\', but there shouldn't be '\' in the end of the make or model of the car. (it should be enough)
+ * Clears received json string from whitespaces and newlines. Doesn't affect
+ * variable scopes. ( '{ "make" : "My Car" }' -> '{"make":"My Car"}' )
+ */
+static void
+clean_json_string (char **string)
+{
+  if (*string == JSON_STR_NULL)
+    return; // Received null pointer.
+  bool var_scope
+      = false; // Flag to check whether whitespace is located inside the
+               // variable scope. (We don't want to ruin the names)
+  int i = 0;
+  int w = j_strlen (*string);
+  char prev_char
+      = ' '; // Not the best way to check '\', but there shouldn't be '\' in
+             // the end of the make or model of the car. (it should be enough)
 
-    for (i = 0; i < w; i++) {
-        //Clear all whitespaces outside the variable scopes.
-        if (isspace(string[0][i]) && !var_scope) {
-            for (int q = i + 1; q <= w; q++) {
-                string[0][q - 1] = string[0][q];
+  for (i = 0; i < w; i++)
+    {
+      // Clear all whitespaces outside the variable scopes.
+      if (j_isspace (string[0][i]) && !var_scope)
+        {
+          for (int q = i + 1; q <= w; q++)
+            {
+              string[0][q - 1] = string[0][q];
             }
-            w--;
-            i--;
+          w--;
+          i--;
         }
-            //It is '"' -> flip variable scope.
-        else if (prev_char != '\\' && string[0][i] == '\"') {
-            if (var_scope) {
-                var_scope = false;
+      // It is '"' -> flip variable scope.
+      else if (prev_char != '\\' && string[0][i] == '\"')
+        {
+          if (var_scope)
+            {
+              var_scope = false;
             }
-            else {
-                var_scope = true;
+          else
+            {
+              var_scope = true;
             }
         }
-        else {
-            prev_char = string[0][i];
+      else
+        {
+          prev_char = string[0][i];
         }
     }
 }
 
-bool json_str_int_from_context(char * json_str, const char * context, int * dest)
+/**
+ * @brief Extract int value from json string
+ *
+ * @param json_str whole json string
+ * @param context context of value
+ * @param dest (int) value to be saved
+ * @return true if found and copied to dest
+ * @return false if failed
+ */
+bool
+json_str_int_from_context (char *json_str, const char *context, int *dest)
 {
-    char *value = json_str;
-    clean_json_string(&value);
-    char *temp = strstr(value, context);
-    if(!temp){
-        return false;
+  char *value = json_str;
+  clean_json_string (&value);
+  char *temp = j_strstr (value, context);
+  if (!temp)
+    {
+      return false;
     }
-    int sign = 1;
-    while (*temp++ != ':');
-    value = temp;
-    int byte_counter = 0;
-    while (*temp != ',' && *temp != '}') {
-        if(*temp == '-'){
-            sign = -1;
-            *value++;
-            byte_counter -= 1;
+  int sign = 1;
+  while (*temp++ != ':')
+    ;
+  value = temp;
+  int byte_counter = 0;
+  while (*temp != ',' && *temp != '}')
+    {
+      if (*temp == '-')
+        {
+          sign = -1;
+          *value++;
+          byte_counter -= 1;
         }
-        temp++;
-        byte_counter++;
+      temp++;
+      byte_counter++;
     }
-    int extracted_value = 0;
-    for (int i = 0; i < byte_counter; ++i) {
-        extracted_value = extracted_value * 10 + *value - '0';
-        value++;
+  int extracted_value = 0;
+  for (int i = 0; i < byte_counter; ++i)
+    {
+      extracted_value = extracted_value * 10 + *value - '0';
+      value++;
     }
-    *dest = extracted_value * sign;
-    return true;
+  *dest = extracted_value * sign;
+  return true;
 }
 
-bool json_str_str_from_context(char * json_str, const char * context, char* dest)
+/**
+ * @brief Extract string value from json string
+ *
+ * @param json_str whole json string
+ * @param context context of value
+ * @param dest (char*) value to be saved
+ * @return true if found and copied to dest
+ * @return false if failed
+ */
+bool
+json_str_str_from_context (char *json_str, const char *context, char *dest)
 {
-    char *value = json_str;
-    clean_json_string(&value);
-    char *temp = strstr(value, context);
-    if(!temp){
-        return false;
+  char *value = json_str;
+  clean_json_string (&value);
+  char *temp = j_strstr (value, context);
+  if (!temp)
+    {
+      return false;
     }
-    while (*temp != ':') temp++;
-    temp += 2;
-    value = temp;
+  while (*temp != ':')
+    temp++;
+  temp += 2;
+  value = temp;
 
-    int byte_counter = 0;
-    while (*temp != '"') {
-        temp++;
-        byte_counter++;
+  int byte_counter = 0;
+  while (*temp != '"')
+    {
+      temp++;
+      byte_counter++;
     }
-    if(dest) memoryset(dest, 0, strlen(dest));
-    memorycpy(value, dest, byte_counter);
-    return true;
+  if (dest)
+    j_memset (dest, 0, j_strlen (dest));
+  j_memcpy (value, dest, byte_counter);
+  return true;
 }
--- json_str/json_str.h
+++ json_str/json_str.h
@@ -5,21 +5,24 @@
 #ifndef JSON_JSON_STRING_H
 #define JSON_JSON_STRING_H
 
-#include <stdbool.h>
 #include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 
 #define JSON_STR_MAX_STRING_LENGTH_FROM_CTX 512
 #define JSON_STR_SEND_MAX_STRING_LEN 1024
 #define JSON_STR_NULL ((char *)0)
 
-typedef struct json_command {
-    unsigned int duration;
-    int speed;
-    int direction;
-}json_command;
+typedef struct json_command
+{
+  unsigned int duration;
+  int speed;
+  int direction;
+} json_command;
 
-bool json_str_int_from_context(char * json_str, const char * context, int * dest);
-bool json_str_str_from_context(char * json_str, const char * context, char * dest);
+bool json_str_int_from_context (char *json_str, const char *context,
+                                int *dest);
+bool json_str_str_from_context (char *json_str, const char *context,
+                                char *dest);
 
-#endif //JSON_JSON_STRING_H
+#endif // JSON_JSON_STRING_H
