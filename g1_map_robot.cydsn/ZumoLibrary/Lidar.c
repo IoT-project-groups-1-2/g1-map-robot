ce1f7d9e7bdf9f0c9935e107c3a85a441045057e
RedHawk <tadshig@gmail.com>
2022-11-20 14:07:00 +0200

    Lidar.c: Better Lidar transaction handling. (#21)

--- g1_map_robot.cydsn/ZumoLibrary/Lidar.c
+++ g1_map_robot.cydsn/ZumoLibrary/Lidar.c
@@ -15,19 +15,25 @@ Lidar_get_distance()
 {
   int distance = -1;
   int count = UART_3_GetRxBufferSize();
+  int i = 0;
+  uint8_t bytes[57];
 
-  if(count)
+  if(count >= 9)
+    for(int q = 0; q < count; ++q)
+      bytes[q] = UART_3_GetByte();
+
+  //It is a bad idea to treat any consequent 0x59 0x59 as a start of UART transaction,
+  //but it will do for now.
+  while (i < count - 8)
   {
-    uint8_t bytes[57];
-    for(int i = 0; i < count; ++i)
+    if (bytes[i] == 0x59 && bytes[i + 1] == 0x59)
     {
-      bytes[i] = UART_3_GetByte();
-    }
-    if(count % 9 == 0 && bytes[0] == 0x59 && bytes[1] == 0x59)
-    {
-      distance = bytes[2] | (bytes[3] << 8);
+      distance = bytes[i + 2] | (bytes[i + 3] << 8);
       old_dist = distance;
+      break;
     }
+    ++i;
   }
+
   return old_dist;
 }
