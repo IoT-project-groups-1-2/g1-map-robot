5a4da70f4079e012d06995eb89756daae2efb828
Vasily Davydov <vasilydavydovmail@yahoo.com>
2022-11-24 09:37:38 +0200

    mauto: wrap gyro z_plane read in a function

---
+++ mauto/balance.c
@@ -0,0 +1,32 @@
+#include "balance.h"
+
+static inline int16_t
+convert_raw (uint8_t high, uint8_t low)
+{
+  return (int16_t) (high << 8) | low;
+}
+
+/**
+ * @brief Get a rounded velocity of gyro's z_plane
+ *
+ * @return int16_t (negative = right, positive = left, 0 = centered)
+ */
+int16_t
+z_plane_get_current ()
+{
+  uint8 z_plane_raw_low, z_plane_raw_high;
+  int16_t z_plane_output = 0, buffer_list = 0;
+
+  I2C_Start ();
+  I2C_Write (GYRO_ADDR, GYRO_CTRL1_REG, GYRO_MODE_ACTIVE);
+  I2C_Write (GYRO_ADDR, GYRO_CTRL4_REG, GYRO_FULL_SCALE_SELECTION);
+
+  for (size_t i = 0; i < (Z_PLANE_DIVISION_COEFFICIENT + 1); i++)
+    {
+      z_plane_raw_low = I2C_Read (GYRO_ADDR, OUT_Z_AXIS_L);
+      z_plane_raw_high = I2C_Read (GYRO_ADDR, OUT_Z_AXIS_H);
+      buffer_list += convert_raw (z_plane_raw_high, z_plane_raw_low);
+      vTaskDelay (5);
+    }
+  return z_plane_output = buffer_list / (Z_PLANE_D_C_SQUARED);
+}
---
+++ mauto/balance.h
@@ -0,0 +1,26 @@
+#ifndef BALANCE_H
+#define BALANCE_H
+
+#include "FreeRTOS.h"
+#include "Gyro.h"
+#include "I2C_Common.h"
+#include <stdlib.h>
+
+/* set gyroscope into active mode */
+#define GYRO_MODE_ACTIVE 0x0F
+/* set full scale selection to 2000dps */
+#define GYRO_FULL_SCALE_SELECTION 0x30
+/* coefficent to divide a list of values with the size of this
+ * 20 is found to be best
+ */
+#define Z_PLANE_DIVISION_COEFFICIENT 0x14
+#define SQUARE(n)                                                             \
+  if (n % 2)                                                                  \
+  ? ((n >> 1) << 2) : (((n >> 1) << 2) + ((n >> 1) << 2) + 1)
+#define Z_PLANE_D_C_SQUARED SQUARE (Z_PLANE_DIVISION_COEFFICIENT)
+
+int16_t z_plane_get_current ();
+
+
+
+#endif /* BALANCE_H */
