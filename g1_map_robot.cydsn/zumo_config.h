b4166088af95a52587b6bcd3be8479497cf6c79d
Evgeny Meshcheryakov <evgeny.meshcheryakov@gmail.com>
2022-11-24 10:23:57 +0200

    main.c: add gyro example. Linking

--- g1_map_robot.cydsn/zumo_config.h
+++ g1_map_robot.cydsn/zumo_config.h
@@ -5,18 +5,19 @@
  * enable simulator mode  */
 #define ZUMO_SIMULATOR 0
 #define LIDAR 0
-#define TEST 1
+#define TEST 0
+#define GYRO 1
 
 /* Change the following definition to 0 to disable MQTT and to 1 to enable MQTT
  */
 #define START_MQTT 1
 
 /* The following definitions must all be in double quotes */
-#define NETWORK_SSID "DavyJones"
+#define NETWORK_SSID "Default"
 
-#define NETWORK_PASSWORD "63PaltsiPhone"
+#define NETWORK_PASSWORD "Pass"
 
-#define MQTT_BROKER "172.20.10.9"
+#define MQTT_BROKER "0.0.0.0.0"
 
 #define MQTT_CLIENT_ID "Zumo101"
 
