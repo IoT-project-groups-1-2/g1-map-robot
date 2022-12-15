b4467a4f5bba5cca23df020a90efe0a826c6488a
RedHawk <tadshig@gmail.com>
2022-12-15 21:27:27 +0200

    Web: Upload newest version of web to repo.
    *Better controls.
    *Deleted redundant routes.

--- Web/index.js
+++ Web/index.js
@@ -1,20 +1,11 @@
 'use strict'
 
-const express = require('express')
-const path = require('path')
-const app = express()
-app.use(express.static(path.join(__dirname, '/')));
+const express = require('express');
+const path = require('path');
+const app = express();
 
 app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname + '/drive.html'));
-})
-
-app.get('/drive', (req, res)=>{
-    res.sendFile(path.join(__dirname + '/drive.html'));
-})
-
-app.get('/map', (req, res)=>{
-    res.sendFile(path.join(__dirname + '/map.html'));
 })
 
 app.listen(3000, () => console.log('Server listening on port 3000'));
